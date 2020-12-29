238ef9f25e048b02d6998c2e0c7cc6ccc4e20b6c
meghna <meghna.tanwal@thoughworks.com>
2020-12-29 12:11:16 +0530

    Meghna | Adds single class imports

--- omod/src/test/java/org/bahmni/module/hip/web/service/CareContextServiceTest.java
+++ omod/src/test/java/org/bahmni/module/hip/web/service/CareContextServiceTest.java
@@ -7,7 +7,10 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CareContextServiceTest {
     private CareContextRepository careContextRepository = mock(CareContextRepository.class);
