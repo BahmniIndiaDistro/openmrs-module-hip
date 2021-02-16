f3f5de85a922d319384040723c31ca90fe43bf31
meghna <meghna.tanwal@thoughworks.com>
2021-02-16 12:07:44 +0530

    [Mahesh|Meghna|Nazeer] adds test for diagnostic report and adds visit type as a search criteria

--- omod/src/main/java/org/bahmni/module/hip/web/controller/DiagnosticReportController.java
+++ omod/src/main/java/org/bahmni/module/hip/web/controller/DiagnosticReportController.java
@@ -1,14 +1,11 @@
 package org.bahmni.module.hip.web.controller;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.bahmni.module.hip.web.client.ClientError;
 import org.bahmni.module.hip.web.model.BundledDiagnosticReportResponse;
 import org.bahmni.module.hip.web.model.DateRange;
 import org.bahmni.module.hip.web.model.DiagnosticReportBundle;
 import org.bahmni.module.hip.web.service.DiagnosticReportService;
 import org.bahmni.module.hip.web.service.ValidationService;
-import org.openmrs.api.PatientService;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
@@ -28,9 +26,6 @@ import static org.bahmni.module.hip.web.utils.DateUtils.parseDate;
 @RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/hip")
 @RestController
 public class DiagnosticReportController extends BaseRestController {
-
-    private Log log = LogFactory.getLog(this.getClass());
-
     private final DiagnosticReportService diagnosticReportService;
     private final ValidationService validationService;
 
@@ -40,21 +35,24 @@ public class DiagnosticReportController extends BaseRestController {
         this.validationService = validationService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/diagnosticReport", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> get(@RequestParam String patientId, @RequestParam String visitType, @RequestParam String fromDate, @RequestParam String toDate) throws ParseException {
-
-            if (patientId == null || patientId.isEmpty())
-                return ResponseEntity.badRequest().body(ClientError.noPatientIdProvided());
-            if (visitType == null || visitType.isEmpty())
-                return ResponseEntity.badRequest().body(ClientError.noVisitTypeProvided());
-            if (!validationService.isValidVisit(visitType))
-                return ResponseEntity.badRequest().body(ClientError.invalidVisitType());
-            if (!validationService.isValidPatient(patientId))
-                return ResponseEntity.badRequest().body(ClientError.invalidPatientId());
-            List<DiagnosticReportBundle> diagnosticReportBundle =
-                    diagnosticReportService.getDiagnosticReports(patientId, new DateRange(parseDate(fromDate), parseDate(toDate)), visitType);
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                    .body(new BundledDiagnosticReportResponse(diagnosticReportBundle));
+    @RequestMapping(method = RequestMethod.GET, value = "/diagnosticReports", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<?> get(@RequestParam String patientId,
+                          @RequestParam String visitType,
+                          @RequestParam String fromDate,
+                          @RequestParam String toDate) throws ParseException {
+        if (patientId == null || patientId.isEmpty())
+            return ResponseEntity.badRequest().body(ClientError.noPatientIdProvided());
+        if (visitType == null || visitType.isEmpty())
+            return ResponseEntity.badRequest().body(ClientError.noVisitTypeProvided());
+        if (!validationService.isValidVisit(visitType))
+            return ResponseEntity.badRequest().body(ClientError.invalidVisitType());
+        if (!validationService.isValidPatient(patientId))
+            return ResponseEntity.badRequest().body(ClientError.invalidPatientId());
+        List<DiagnosticReportBundle> diagnosticReportBundle =
+                diagnosticReportService.getDiagnosticReports(patientId, new DateRange(parseDate(fromDate), parseDate(toDate)), visitType);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(new BundledDiagnosticReportResponse(diagnosticReportBundle));
     }
 }
--- omod/src/main/java/org/bahmni/module/hip/web/service/DiagnosticReportService.java
+++ omod/src/main/java/org/bahmni/module/hip/web/service/DiagnosticReportService.java
@@ -1,6 +1,5 @@
 package org.bahmni.module.hip.web.service;
 
-import org.apache.log4j.Logger;
 import org.bahmni.module.hip.web.model.DateRange;
 import org.bahmni.module.hip.web.model.DiagnosticReportBundle;
 import org.bahmni.module.hip.web.model.OpenMrsPrescription;
@@ -8,36 +7,37 @@ import org.openmrs.Encounter;
 import org.openmrs.EncounterType;
 import org.openmrs.Obs;
 import org.openmrs.Patient;
+import org.openmrs.VisitType;
 import org.openmrs.api.EncounterService;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.VisitService;
 import org.openmrs.parameter.EncounterSearchCriteria;
 import org.openmrs.parameter.EncounterSearchCriteriaBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class DiagnosticReportService {
-    private static final Logger log = Logger.getLogger(PrescriptionService.class);
-
-    private final OpenMRSDrugOrderClient openMRSDrugOrderClient;
     private final FhirBundledDiagnosticReportBuilder fhirBundledDiagnosticReportBuilder;
     private final PatientService patientService;
     private final EncounterService encounterService;
-
-
+    private final VisitService visitService;
 
     @Autowired
-    public DiagnosticReportService(OpenMRSDrugOrderClient openMRSDrugOrderClient,
-                                   FhirBundledDiagnosticReportBuilder fhirBundledDiagnosticReportBuilder,
+    public DiagnosticReportService(FhirBundledDiagnosticReportBuilder fhirBundledDiagnosticReportBuilder,
                                    PatientService patientService,
-                                   EncounterService encounterService) {
-        this.openMRSDrugOrderClient = openMRSDrugOrderClient;
+                                   EncounterService encounterService, VisitService visitService) {
         this.fhirBundledDiagnosticReportBuilder = fhirBundledDiagnosticReportBuilder;
         this.patientService = patientService;
         this.encounterService = encounterService;
+        this.visitService = visitService;
     }
 
     public List<DiagnosticReportBundle> getDiagnosticReports(String patientUuid, DateRange dateRange, String visitType) {
@@ -51,27 +51,39 @@ public class DiagnosticReportService {
         encounterTypes.add(encounterService.getEncounterType("RADIOLOGY"));
         encounterTypes.add(encounterService.getEncounterType("Patient Document"));
 
-        HashMap<Encounter, List<Obs>> encounterListMap = new HashMap<>();
-
-        if (patient != null) {
-            EncounterSearchCriteriaBuilder encounterSearchCriteriaBuilder = new EncounterSearchCriteriaBuilder()
-                    .setPatient(patient).setFromDate(fromDate).
-                    setToDate(toDate).setIncludeVoided(false);
-            encounterSearchCriteriaBuilder.setEncounterTypes(encounterTypes);
-
-            EncounterSearchCriteria encounterSearchCriteria = encounterSearchCriteriaBuilder.createEncounterSearchCriteria();
-
-            List<Encounter> encounters = encounterService.getEncounters(encounterSearchCriteria);
-
-            for (Encounter e : encounters) {
-                encounterListMap.put(e, new ArrayList<>(e.getAllObs()));
-            }
-        }
+        HashMap<Encounter, List<Obs>> encounterListMap = getAllObservations(fromDate, toDate, patient, encounterTypes, visitType);
         List<OpenMrsPrescription> openMrsPrescriptions = OpenMrsPrescription.fromDiagnosticReport(encounterListMap);
 
         return openMrsPrescriptions
                 .stream()
                 .map(fhirBundledDiagnosticReportBuilder::fhirBundleResponseFor)
                 .collect(Collectors.toList());
+    }
+
+    private HashMap<Encounter, List<Obs>> getAllObservations(Date fromDate, Date toDate,
+                                                             Patient patient,
+                                                             List<EncounterType> encounterTypes,
+                                                             String visitType) {
+        HashMap<Encounter, List<Obs>> encounterListMap = new HashMap<>();
+        VisitType visitTypeFromService = new VisitType();
+        List<VisitType> visitTypes = visitService.getAllVisitTypes();
+        for (VisitType vt : visitTypes) {
+            if (vt.getName().toLowerCase().equals(visitType.toLowerCase()))
+                visitTypeFromService = vt;
+        }
+        EncounterSearchCriteriaBuilder encounterSearchCriteriaBuilder = new EncounterSearchCriteriaBuilder()
+                .setPatient(patient)
+                .setFromDate(fromDate)
+                .setToDate(toDate)
+                .setIncludeVoided(false)
+                .setEncounterTypes(encounterTypes)
+                .setVisitTypes(Collections.singletonList(visitTypeFromService));
+
+        EncounterSearchCriteria encounterSearchCriteria = encounterSearchCriteriaBuilder.createEncounterSearchCriteria();
+        List<Encounter> encounters = encounterService.getEncounters(encounterSearchCriteria);
+        for (Encounter e : encounters) {
+            encounterListMap.put(e, new ArrayList<>(e.getAllObs()));
+        }
+        return encounterListMap;
     }
 }
