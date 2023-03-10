1f013b56d6b5ebc13abf2c5cbb447889b0f5a193
SanoferSameera <sanofersameera25@gmail.com>
2023-03-10 14:36:03 +0530

    BAH-2913 | Refactor. care-context to have visit uuid and update care-context name

--- api/src/main/java/org/bahmni/module/hip/api/dao/Constants.java
+++ api/src/main/java/org/bahmni/module/hip/api/dao/Constants.java
@@ -2,7 +2,7 @@ package org.bahmni.module.hip.api.dao;
 
 public class Constants {
 
-    public static final String VISIT_TYPE = "VISIT_TYPE";
+    public static final String VISIT_TYPE = "VISIT";
     public static final String PROGRAM = "PROGRAM";
     public static final String ORDER_ACTION = "DISCONTINUE";
 }
