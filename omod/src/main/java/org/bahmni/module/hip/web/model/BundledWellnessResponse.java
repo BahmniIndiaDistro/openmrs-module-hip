4e57e587318abdf14551cf3bb143ddf16e3ebbd0
Abinaya U <abinaya.u@thoughtworks.com>
2023-04-21 16:14:17 +0530

    fix. wellness bundle entry should match with hi type

--- omod/src/main/java/org/bahmni/module/hip/web/model/BundledWellnessResponse.java
+++ omod/src/main/java/org/bahmni/module/hip/web/model/BundledWellnessResponse.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Getter
 @Setter
 public class BundledWellnessResponse {
-    private List<WellnessRecordBundle> wellnessRecordBundles;
+    private List<WellnessRecordBundle> wellnessRecord;
 }
