8031da4310086097359bb4f5fb9c8a41e60ef4b0
SanoferSameera <sanofersameera25@gmail.com>
2023-10-05 18:14:27 +0530

    BAH-3071 | Add. api to get lgdcode for state and district

---
+++ omod/src/main/java/org/bahmni/module/hip/web/exception/LGDCodeNotFoundException.java
@@ -0,0 +1,8 @@
+package org.bahmni.module.hip.web.exception;
+
+public class LGDCodeNotFoundException extends Exception {
+
+    public LGDCodeNotFoundException(String message) {
+        super(message);
+    }
+}
